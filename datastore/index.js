8c4701e3d244ac5a16184740d932612e7b516535
Jackson Pham <jackson.pham@gmail.com>
2021-03-30 16:32:40 -0700

    Completed Update

--- datastore/index.js
+++ datastore/index.js
@@ -61,19 +61,18 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  // var item = items[id];
-  // if (!item) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   items[id] = text;
-  //   callback(null, { id, text });
-  // }
-  fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, fileData) => {
     if (err) {
-      // console.log('This is Our iD', id);
       callback(new Error(`No item with id: ${id}`));
     } else {
-      callback(null, {id, text});
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          // console.log('This is Our iD', id);
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback(null, {id, text});
+        }
+      });
     }
   });
 };
