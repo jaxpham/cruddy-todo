8c9f754fe2380f2381e6c61ac64939cc80b500bd
Jackson Pham <jackson.pham@gmail.com>
2021-03-30 14:25:45 -0700

    Completed 2 Checkmarks With Update

--- datastore/index.js
+++ datastore/index.js
@@ -61,14 +61,24 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
+  fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+    if (err) {
+      // console.log('This is Our iD', id);
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, {id, text});
+    }
+  });
 };
+
+
 
 exports.delete = (id, callback) => {
   var item = items[id];
