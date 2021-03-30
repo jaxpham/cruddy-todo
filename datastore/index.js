eff520eeb359d409ad9fc7df1adcb5d7a35fa1d5
Jackson Pham <jackson.pham@gmail.com>
2021-03-30 16:53:24 -0700

    Completed Delete

--- datastore/index.js
+++ datastore/index.js
@@ -77,17 +77,28 @@ exports.update = (id, text, callback) => {
   });
 };
 
-
-
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+
+  // Locate file with that id
+  // if err
+  // callback callback(new Error(`No item with id: ${id}`));
+  // else
+  // delete the file the file
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
